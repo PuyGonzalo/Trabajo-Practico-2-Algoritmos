f464d399a61f2a84a41fd23abd76c93a95f30a17
gongus <30372906+gongus@users.noreply.github.com>
2018-06-24 18:25:31 -0300

    Add files via upload

---
+++ vector_aux.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+#include "types.h"
+#include "vector.h"
+
+status_t ADT_vector_export_as_CSV (const ADT_vector_t * p, FILE * fo, char delimitter, status_t (*pf)(const void *, FILE *, char))
+{
+	size_t i;
+	status_t st;
+
+	if (p == NULL || fo == NULL || pf == NULL)
+		return ERROR_NULL_POINTER;
+
+	for (i=0 ; i < size ; i++)
+	{
+		st= (*pf)(p-> elements[i],delimitter, fo);
+		
+		if (st != OK)
+			return st;
+	}
+
+	return OK;
+}
