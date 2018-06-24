9eb1a1055c226f3d287c041019f5f24879a9412d
Maxi <40548274+maxtecnon@users.noreply.github.com>
2018-06-24 18:43:41 -0300

    Update vector_aux.c

--- vector_aux.c
+++ vector_aux.c
@@ -10,15 +10,13 @@ status_t ADT_Vector_export_as_CSV (const ADT_Vector_t * p, FILE * fo, char delim
 	size_t i;
 	status_t st;
 
-	if (p == NULL || fo == NULL || pf == NULL)
+	if (p==NULL || fo==NULL || pf==NULL)
 		return ERROR_NULL_POINTER;
 
-	for (i=0 ; i < size ; i++)
+	for (i=0;i<size;i++)
 	{
-		st= (*pf)(p-> elements[i],delimitter, fo);
-		
-		if (st != OK)
-			return st;
+		if((st=(*pf)(p->element[i],delimitter, fo))!=OK)
+	           return st;
 	}
 
 	return OK;
