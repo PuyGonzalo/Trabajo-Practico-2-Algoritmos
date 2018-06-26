8bb49c84bba9131400674760066b1c5e80a9ddaa
gongus <30372906+gongus@users.noreply.github.com>
2018-06-26 20:36:57 -0300

    Update error.h

--- error.h
+++ error.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include "types.h"
 
-status_t print_error (FILE * stream, status_t st);
+status_t print_error (FILE * f, status_t st);
 
-#define MAX_ERRORS 
+#define MAX_ERRORS  /* FALTA AGREGAR EL NUMERO DE ERRORES FINAL */
 
 #endif
