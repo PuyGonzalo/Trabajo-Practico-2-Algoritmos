daa6363d183fd60168878dc96d5ef145db9c3c7b
Maxi <40548274+maxtecnon@users.noreply.github.com>
2018-06-25 23:50:09 -0300

    Add files via upload

---
+++ utilities.c
@@ -0,0 +1,33 @@
+/***********************************************************************
+Facultad de Ingeniería
+Universidad de Buenos Aires
+Algoritmos y programacion I (95.11)
+Profesor: Ing. Martín Cardozo
+Alumno: Puy, Gonzalo - Reigada, Maximiliano Daniel
+Archivo: utilities.c
+Descripción: Contiene función de manejo de arreglos.
+************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+#include "utilities.h"
+
+/*************Comienzo de función de clonado de arreglos*************/
+string strdupl(const string s)
+{
+	char *t;
+	size_t i;
+
+	if(s==NULL)
+	   return NULL;
+
+	if((t=(char*)malloc((strlen(s)+1)*sizeof(char)))==NULL)
+           return NULL;
+
+	for (i=0;(t[i]=s[i]);i++);
+
+        return t;
+}
+/***************Final de función de clonado de arreglos***************/
