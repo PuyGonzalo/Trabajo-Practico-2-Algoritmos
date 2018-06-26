16f6d87f2e74cede2064e0d92cc33be4037aaacc
Maxi <40548274+maxtecnon@users.noreply.github.com>
2018-06-26 20:34:02 -0300

    Add files via upload

---
+++ mp3explorer.h
@@ -0,0 +1,22 @@
+/***********************************************************************
+Facultad de Ingeniería
+Universidad de Buenos Aires
+Algoritmos y programacion I (95.11)
+Profesor: Ing. Martín Cardozo
+Alumno: Puy, Gonzalo - Reigada, Maximiliano Daniel
+Archivo: mp3explorer.h
+Descripción: Contiene datos referidos a funciones de procesamiento de mp3.
+************************************************************************/
+#ifndef MP3EXPLORER__H
+#define MP3EXPLORER__H
+
+#include <stdio.h>
+#include "types.h"
+
+#define CSV_DELIMITER '|'
+
+/**********************************PROTOTYPES***********************************/
+status_t process_mp3_files (char *[]);
+/**********************************PROTOTYPES***********************************/
+
+#endif
