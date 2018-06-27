9006ffabf9acb496ac864cb1ae8af5221e470618
Maxi <40548274+maxtecnon@users.noreply.github.com>
2018-06-27 04:02:24 -0300

    Update error.h

--- error.h
+++ error.h
@@ -1,3 +1,13 @@
+/*************************************************************
+Facultad de Ingeniería
+Universidad de Buenos Aires
+Algoritmos y programacion I (95.11)
+Profesor: Ing. Martín Cardozo
+Alumno: Puy, Gonzalo - Reigada, Maximiliano Daniel
+Archivo: errors.c
+Descripción: Contiene diccionario de errores.
+*************************************************************/
+
 #ifndef ERROR__H
 #define ERROR__H
 
