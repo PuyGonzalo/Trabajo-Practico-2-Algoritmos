0b117337d678dc64c6efc84671239ea2e305505a
gongus <30372906+gongus@users.noreply.github.com>
2018-06-27 20:47:25 -0300

    Add files via upload

---
+++ codigo/error.h
@@ -0,0 +1,23 @@
+/*******************************************************************
+Facultad de Ingeniería
+Universidad de Buenos Aires
+Algoritmos y programacion I (95.11)
+Profesor: Ing. Martín Cardozo
+Alumnos: Puy, Gonzalo - Reigada, Maximiliano Daniel
+Archivo: errors.h
+Descripción: Contiene datos relacionados al diccionario de errores.
+*******************************************************************/
+
+#ifndef ERROR__H
+#define ERROR__H
+
+#include <stdio.h>
+#include "types.h"
+
+#define MAX_ERRORS 13
+
+
+/****************PROTOTIPOS*********************/
+status_t print_error (FILE * f, status_t st);
+/****************PROTOTIPOS*********************/
+#endif
